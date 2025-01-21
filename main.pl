72567a224801785d349ef2f606087b4d0f242d04
MERRET Loic <Loic.Merret@bordeaux-inp.fr>
2025-01-21 11:28:23 +0100

    last version of main

--- main.pl
+++ main.pl
@@ -1,18 +1,31 @@
+:- use_module(library(socket)).
+:- use_module(library(http/json)).
+
+
+% --- Définition de l utilisateur type ---
+
 % Utilisateur type
 utilisateur_type(
     25,          % Age (adulte)
     homme,       % Sexe
-    70,          % Poids (en kg)
+    150,          % Poids (en kg)
+    1.70,         % Taille (en M)
     8,           % Sommeil (en heures)
     120,         % Activité modérée (en minutes par semaine)
     50,          % Activité intense (en minutes par semaine)
     2,           % Jours de musculation
     2800,        % Calories consommées
-    3            % Eau consommée (en litres)
+    3,            % Eau consommée (en litres)
+    non,          % Tabac
+    non,          % Alcool
+    equilibree,   % Alimentation
+    80,           % Frequence cardiaque
+    16,           % VitamineD consomme
+    4,           % TempsExposition
+    grande        % Surface exposée
 ).
 
-
-% Définition des recommandations de sommeil en fonction des groupes d âge
+% --- Recommandations de sommeil ---
 
 % Adultes de 18 à 64 ans : 7 à 9 heures de sommeil par nuit
 recommandation_sommeil(Age, Sommeil_min, Sommeil_max) :-
@@ -34,6 +47,8 @@ recommandation_sommeil(Age, Sommeil_min, Sommeil_max) :-
     Age >= 6, Age =< 13,
     Sommeil_min = 9, Sommeil_max = 11.
 
+% --- Vérification du sommeil ---
+
 % Règle principale pour vérifier la conformité au temps de sommeil recommandé
 verifier_sommeil(Age, Sommeil, Message) :-
     recommandation_sommeil(Age, Sommeil_min, Sommeil_max),
@@ -44,199 +59,67 @@ verifier_sommeil(Age, Sommeil, Message) :-
     ;   Message = 'Votre temps de sommeil respecte les recommandations pour votre âge. Continuez ainsi pour une bonne santé !'
     ).
 
-% Exemple de consultation sommeil:
-% ?- verifier_sommeil(25, 6, Message).
-% Message = 'Vous n\'avez pas assez dormi. Essayez d\'augmenter votre temps de sommeil pour atteindre au moins 7 heures.'
+% --- Définition des recommandations d activité ---
 
-% ?- verifier_sommeil(25, 8, Message).
-% Message = 'Votre temps de sommeil respecte les recommandations pour votre âge. Continuez ainsi pour une bonne santé !'
-
-% ?- verifier_sommeil(25, 10, Message).
-% Message = 'Vous avez dormi plus que le temps recommandé. Essayez de limiter votre sommeil à un maximum de 9 heures.'
-
-
-
-% Recommandations d activité physique
-
-% Recommandations pour les adultes (18 ans et plus)
-recommandation_activite(Age, Activite_min, Activite_intense_min, Musculation_jours) :-
-    Age >= 18,
-    Activite_min = 150,            % 150 minutes d activité modérée par semaine
-    Activite_intense_min = 75,     % ou 75 minutes d activité intense par semaine
-    Musculation_jours = 2.         % 2 jours de musculation par semaine
-
-% Recommandations pour les enfants et adolescents (6 à 17 ans)
-recommandation_activite(Age, Activite_min, Activite_intense_jours, Musculation_jours) :-
-    Age >= 6, Age =< 17,
-    Activite_min = 60,             % 60 minutes d activité physique par jour
-    Activite_intense_jours = 3,    % avec des activités intenses 3 jours par semaine
-    Musculation_jours = 0.         % pas de recommandations spécifiques pour la musculation
-
-% Vérification des activités physiques avec des recommandations personnalisées
-verifier_activite(Age, Activite_effectuee, Activite_intense, Musculation) :-
-    recommandation_activite(Age, Activite_min, Activite_intense_min, Musculation_jours),
-    (   % Vérifier l activité modérée ou intense
-        Activite_effectuee < Activite_min,
-        Manque_activite is Activite_min - Activite_effectuee,
-        format('Vous avez besoin d\'au moins ~d minutes supplémentaires d\'activité modérée pour atteindre les ~d minutes hebdomadaires recommandées.~n', [Manque_activite, Activite_min]),
-        fail
-    ;   Activite_intense < Activite_intense_min,
-        Manque_intense is Activite_intense_min - Activite_intense,
-        format('Vous avez besoin d\'au moins ~d minutes supplémentaires d\'activité intense pour atteindre les ~d minutes hebdomadaires recommandées.~n', [Manque_intense, Activite_intense_min]),
-        fail
-    ;   Musculation < Musculation_jours,
-        Manque_musculation is Musculation_jours - Musculation,
-        format('Il est recommandé de faire de la musculation au moins ~d jours par semaine. Essayez d\'ajouter ~d jour(s) supplémentaire(s) de musculation.~n', [Musculation_jours, Manque_musculation]),
-        fail
+recommandation_activite(Age, Activite_min) :-
+    (   Age >= 18
+    ->  Activite_min = 150  % Minutes par semaine pour les adultes
+    ;   Age >= 6, Age =< 17
+    ->  Activite_min = 60   % Minutes par jour pour les enfants et adolescents
+    ;   throw(error(invalid_age(Age), 'Age must be 6 or above'))
     ).
 
-verifier_activite(Age, Activite_effectuee, Activite_intense, Musculation) :-
-    recommandation_activite(Age, Activite_min, Activite_intense_min, Musculation_jours),
-    % Si aucune condition d échec n est remplie, on affiche le message de réussite
-    Activite_effectuee >= Activite_min,
-    Activite_intense >= Activite_intense_min,
-    Musculation >= Musculation_jours,
-    format('Bravo ! Vous respectez les recommandations d\'activité physique pour votre âge. Continuez ainsi !~n', []).
 
-% Vérification pour les enfants et adolescents (sans musculation)
-verifier_activite(Age, Activite_effectuee, Activite_intense_jours, _) :-
-    Age >= 6, Age =< 17,
-    recommandation_activite(Age, Activite_min, Activite_intense_jours_rec, _),
-    (   Activite_effectuee < Activite_min,
-        Manque_activite is Activite_min - Activite_effectuee,
-        format('Vous avez besoin d\'au moins ~d minutes supplémentaires d\'activité physique quotidienne pour atteindre les ~d minutes recommandées.~n', [Manque_activite, Activite_min]),
-        fail
-    ;   Activite_intense_jours < Activite_intense_jours_rec,
-        Manque_intense_jours is Activite_intense_jours_rec - Activite_intense_jours,
-        format('Vous avez besoin d\'au moins ~d jour(s) supplémentaire(s) d\'activité intense par semaine pour atteindre les ~d jours recommandés.~n', [Manque_intense_jours, Activite_intense_jours_rec]),
-        fail
+% --- Vérification des activités physiques ---
+verifier_activite(Age, Activite_effectuee, Message) :-
+    recommandation_activite(Age, Activite_min),
+    (   Activite_effectuee < Activite_min
+    ->  Message = 'L activité physique est importante pour l équilibre physique et morale ! Encore un petit effort.'
+    ;   Message = 'Bravo ! Vous respectez les recommandations d\'activité physique.'
     ).
-verifier_activite(Age, Activite_effectuee, Activite_intense_jours, _) :-
-    Age >= 6, Age =< 17,
-    recommandation_activite(Age, Activite_min, Activite_intense_jours_rec, _),
-    % Si aucune condition d échec n est remplie, on affiche le message de réussite
-    Activite_effectuee >= Activite_min,
-    Activite_intense_jours >= Activite_intense_jours_rec,
-    format('Bravo ! Vous respectez les recommandations d\'activité physique pour votre âge. Continuez ainsi !~n', []).
 
-% Exemples de consultations :
-% ?- verifier_activite(25, 100, 60, 1).
-% Vous avez besoin d au moins 50 minutes supplémentaires d activité modérée pour atteindre les 150 minutes hebdomadaires recommandées.
 
-% ?- verifier_activite(25, 160, 80, 2).
-% Bravo ! Vous respectez les recommandations d activité physique pour votre âge. Continuez ainsi !
 
-% ?- verifier_activite(12, 50, 2, 0).
-% Vous avez besoin d au moins 10 minutes supplémentaires d activité physique quotidienne pour atteindre les 60 minutes recommandées.
+% --- Recommandations caloriques ---
 
-% Recommandations caloriques quotidiennes (en tenant compte du poids)
-
-% Calcul des besoins caloriques en fonction du poids (avec un facteur d ajustement simplifié)
+% Calcul des besoins caloriques en fonction du poids
 calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max) :-
     (   Poids < 50
-    ->  Kcal_min is 1800, Kcal_max is 1900 ;   % Moins de calories pour un poids faible
-        Poids >= 50, Poids =< 75
-    ->  Kcal_min is 2000, Kcal_max is 2200 ;   % Plage standard pour poids moyen
-        Poids > 75
-    ->  Kcal_min is 2200, Kcal_max is 2500   % Plus de calories pour un poids élevé
+    ->  Kcal_min = 1800, Kcal_max = 1900
+    ;   Poids =< 75
+    ->  Kcal_min = 2000, Kcal_max = 2200
+    ;   Kcal_min = 2200, Kcal_max = 2500
     ).
 
-% Adultes hommes de 18 à 64 ans : ajustement en fonction du poids
+% Recommandations caloriques selon âge et sexe
 recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = homme,
-    Age >= 18, Age =< 64,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
+    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max),
+    (   Sexe = homme, Age >= 18, Age =< 64
+    ;   Sexe = femme, Age >= 18, Age =< 64
+    ;   Age >= 65
+    ;   Age >= 14, Age =< 17
+    ;   Age >= 6, Age =< 13
+    ).
 
-% Adultes femmes de 18 à 64 ans : ajustement en fonction du poids
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = femme,
-    Age >= 18, Age =< 64,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-% Personnes âgées de 65 ans et plus
-% Hommes : ajustement en fonction du poids
-% Femmes : ajustement en fonction du poids
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = homme,
-    Age >= 65,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = femme,
-    Age >= 65,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-% Adolescents de 14 à 17 ans
-% Hommes : ajustement en fonction du poids
-% Femmes : ajustement en fonction du poids
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = homme,
-    Age >= 14, Age =< 17,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = femme,
-    Age >= 14, Age =< 17,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-% Enfants de 6 à 13 ans
-% Ajustement en fonction du poids pour garçons et filles
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = homme,
-    Age >= 6, Age =< 13,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max) :-
-    Sexe = femme,
-    Age >= 6, Age =< 13,
-    calcule_besoins_caloriques(Poids, Kcal_min, Kcal_max).
-
-% Vérification des calories consommées en fonction des recommandations
+% Vérification des calories consommées
 verifier_kcal(Age, Sexe, Poids, Kcal_consomme, Message) :-
     recommandation_kcal(Age, Sexe, Poids, Kcal_min, Kcal_max),
     (   Kcal_consomme < Kcal_min
-    ->  Message = 'Vous n avez pas consommé assez de calories. Essayez d augmenter votre apport calorique.'
+    ->  Message = 'Augmentez votre apport calorique.'
     ;   Kcal_consomme > Kcal_max
-    ->  Message = 'Vous avez consommé trop de calories. Essayez de réduire votre apport calorique.'
-    ;   Message = 'Votre apport calorique est adapté à votre âge, sexe et poids. Continuez ainsi pour une bonne santé !'
+    ->  Message = 'Réduisez votre apport calorique.'
+    ;   Message = 'Votre apport calorique est adapté. Continuez ainsi !'
     ).
 
-% Exemple de consultation :
-% ?- verifier_kcal(25, homme, 70, 2500, Message).
-% Message = 'Vous avez consommé trop de calories. Essayez de réduire votre apport calorique.'
+% --- Recommandations d eau ---
 
-% ?- verifier_kcal(70, femme, 80, 2300, Message).
-% Message = 'Votre apport calorique est adapté à votre âge, sexe et poids. Continuez ainsi pour une bonne santé !'
-
-% ?- verifier_kcal(17, homme, 60, 1900, Message).
-% Message = 'Vous n avez pas consommé assez de calories. Essayez d augmenter votre apport calorique.'
-
-% Définition des recommandations en therme de besoin d eau en fonction de l âge et du sexe
-
-% Adultes : hommes (18 ans et plus) ont besoin de 3,5 litres d eau par jour
+% Recommandations générales pour adultes
 recommandation_eau(Age, Eau_min, Eau_max) :-
-    Age >= 18,
-    Eau_min = 3.5, Eau_max = 3.5.
-
-% Adultes : femmes (18 ans et plus) ont besoin de 2,5 litres d eau par jour
-recommandation_eau(Age, Eau_min, Eau_max) :-
-    Age >= 18,
-    Eau_min = 2.5, Eau_max = 2.5.
-
-% Adolescents (14-17 ans) : 2 litres d eau par jour
-recommandation_eau(Age, Eau_min, Eau_max) :-
-    Age >= 14, Age =< 17,
-    Eau_min = 2, Eau_max = 2.
-
-% Enfants (6-13 ans) : 1,5 à 2 litres d eau par jour
-recommandation_eau(Age, Eau_min, Eau_max) :-
-    Age >= 6, Age =< 13,
-    Eau_min = 1.5, Eau_max = 2.
-
-% Personnes âgées de 65 ans et plus : 2 à 2,5 litres d eau par jour
-recommandation_eau(Age, Eau_min, Eau_max) :-
-    Age >= 65,
-    Eau_min = 2, Eau_max = 2.5.
+    (   Age >= 18
+    ->  Eau_min = 3, Eau_max = 5
+    ;   Age < 18
+    ->  Eau_min = 2, Eau_max = 3
+    ).
 
 % Règle principale pour vérifier la conformité à la quantité d eau recommandée
 verifier_eau(Age, Eau, Message) :-
@@ -303,47 +186,127 @@ synthese_vitamine_d(TempsExposition, SurfaceExposee, VitamineD_synthetisee) :-
     VitamineD_synthetisee is 10 * (TempsExposition / 15) * FacteurSurface.
 
 % Vérification de l apport en vitamine D avec prise en compte de l exposition au soleil
-verifier_vitamine_d(Age, VitamineD_consomme, TempsExposition, SurfaceExposee) :-
+verifier_vitamine_d(Age, VitamineD_consomme, TempsExposition, SurfaceExposee, Message) :-
     recommandation_vitamine_d(Age, VitamineD_recommandee),
     synthese_vitamine_d(TempsExposition, SurfaceExposee, VitamineD_synthetisee),
     TotalVitamineD is VitamineD_consomme + VitamineD_synthetisee,
     (   TotalVitamineD < VitamineD_recommandee
     ->  Manque is VitamineD_recommandee - TotalVitamineD,
-        format('Vous manquez de ~f µg de vitamine D. Augmentez votre exposition au soleil ou consommez des aliments riches en vitamine D.', [Manque]),
-        fail
-    ;   format('Votre apport en vitamine D est suffisant. Continuez à maintenir cet équilibre nutritionnel.', [])
+        Message = 'Vous manquez de vitamine D. Augmentez votre exposition au soleil ou consommez des aliments riches en vitamine D.'
+    ;   Message = 'Votre apport en vitamine D est suffisant. Continuez à maintenir cet équilibre nutritionnel.'
+    ).
+
+% Calcul de l IMC
+
+calcul_imc(Poids, Taille, IMC) :-
+    Taille > 0,
+    Poids > 0,
+    IMC is  Poids / (Taille * Taille).
+
+% Définition des plages d IMC selon l âge
+plage_imc(Age, IMC_min, IMC_max) :-
+    (   Age >= 18, Age =< 64
+    ->  IMC_min = 18.5, IMC_max = 24.9
+    ;   Age >= 65
+    ->  IMC_min = 22, IMC_max = 27
+    ;   Age >= 2, Age =< 17
+    ->  IMC_min >= 18, IMC_max = 24
+    ).
+
+% Règles combinées pour évaluer la santé d une personne
+
+% Base de connaissances pour des seuils recommandés
+recommandation_sommeil(Age, Sommeil_min, Sommeil_max). % heures par nuit
+recommandation_activite(Age, Activite_min). % minutes par semaine
+calcul_imc(Poids,Taille,IMC).
+plage_imc(Age, IMC_min, IMC_max).
+
+% Facteurs de risque : tabac, alcool, alimentation déséquilibrée
+facteur_risque(tabac).
+facteur_risque(alcool).
+facteur_risque(alimentation_desequilibree).
+
+% Règles pour évaluer les risques de santé
+risque_obesite(IMC) :-
+    IMC > 30.
+risque_infarctus(IMC, Activite, Tabac) :-
+    (IMC > 30; Activite < 150; Tabac == oui).
+risque_cancer(Tabac, Alcool, Alimentation) :-
+    (Tabac == oui; Alcool == oui; Alimentation == desequilibree).
+
+% Règle principale : bonne santé
+recommandation_sante(Sommeil, Activite, IMC, Tabac, Alcool, Alimentation) :-
+    utilisateur_type(Age, Sexe, Poids, Taille, Sommeil, Activite, Activite_intense, Musculation, Kcal, Eau, Tabac, Alcool, Alimentation, Freq_cardiaque, VitamineD_consomme, TempsExposition, SurfaceExposee),
+    recommandation_sommeil(Age, Sommeil_min, Sommeil_max),
+    Sommeil >= Sommeil_min, Sommeil =< Sommeil_max,
+    recommandation_activite(Age, Activite_min),
+    Activite >= Activite_min,
+    calcul_imc(Poids,Taille,IMC),
+    plage_imc(Age, IMC_min, IMC_max),
+    IMC >= IMC_min, IMC =< IMC_max.
+
+% Messages détaillés sur l état de santé
+verifier_sante(Sommeil, Activite, IMC, Tabac, Alcool, Alimentation, Message) :-
+    (   recommandation_sante(Sommeil, Activite, IMC, Tabac, Alcool, Alimentation)
+    ->  Message = 'You are in good health! Keep maintaining a balanced lifestyle.'
+    ;   (   risque_obesite(IMC)
+        ->  Message = 'Risk of obesity detected. Consider adopting a healthier diet and increasing physical activity.'
+        ;   (   risque_infarctus(IMC, Activite, Tabac)
+            ->  Message = 'Risk of heart disease detected. Monitor your weight, increase physical activity, and avoid smoking.'
+            ;   (   risque_cancer(Tabac, Alcool, Alimentation)
+                ->  Message = 'Risk of cancer detected. Avoid tobacco and alcohol, and focus on a balanced diet.'
+                ;   Message = 'No specific health risks detected, but some areas may need attention. Keep monitoring your health!'
+                )
+            )
+        )
     ).
 
 
 
 
-% Fonction qui teste toutes les règles pour l utilisateur type
 tester_regles :-
-    utilisateur_type(Age, Sexe, Poids, Sommeil, Activite, Activite_intense, Musculation, Kcal, Eau),
-    
+    utilisateur_type(Age, Sexe, Poids, Taille, Sommeil, Activite, Activite_intense, Musculation, Kcal, Eau, Tabac, Alcool, Alimentation, Freq_cardiaque, VitamineD_consomme, TempsExposition, SurfaceExposee),
+
+    % Calcul de l IMC
+    calcul_imc(Poids, Taille, IMC),
+    format('IMC: ~w~n', [IMC]),
+
     % Vérification du sommeil
     verifier_sommeil(Age, Sommeil, MessageSommeil),
     format('Sommeil: ~w~n', [MessageSommeil]),
-    
+
     % Vérification de l activité physique
-    verifier_activite(Age, Activite, Activite_intense, Musculation),
+    verifier_activite(Age, Activite, MessageActivite),
     format('Activités: ~w~n', [MessageActivite]),
-    
+
     % Vérification des calories
     verifier_kcal(Age, Sexe, Poids, Kcal, MessageKcal),
     format('Calories: ~w~n', [MessageKcal]),
-    
+
     % Vérification de l hydratation
     verifier_eau(Age, Eau, MessageEau),
     format('Eau: ~w~n', [MessageEau]),
 
+    % Vérification du stress
+    verifier_stress(Age, Freq_cardiaque, MessageStress),
+    format('Stress: ~w~n', [MessageStress]),
 
+    % Vérification de la vitamine D
+    verifier_vitamine_d(Age, VitamineD_consomme, TempsExposition, SurfaceExposee, MessageVitamineD),
+    format('Vitamine D: ~w~n', [MessageVitamineD]),
+
+    % Vérification de la santé globale
+    verifier_sante(Sommeil, Activite, IMC, Tabac, Alcool, Alimentation, MessageSante),
+    format('Santé: ~w~n', [MessageSante]),
+
+    % Résultat global sous forme de liste
     Result = [
         ['Sommeil', MessageSommeil],
         ['Activités', MessageActivite],
         ['Calories', MessageKcal],
-        ['Eau', MessageEau]
-    ].
-
-
-
+        ['Eau', MessageEau],
+        ['Stress', MessageStress],
+        ['Vitamine D', MessageVitamineD],
+        ['Bilan santé', MessageSante]
+    ],
+    format('Résultat global : ~w~n', [Result]).
